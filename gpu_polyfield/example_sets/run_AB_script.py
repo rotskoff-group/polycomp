7ce603d039c48231e0933997e1001f10edbd8686
EmmitPert <epert@stanford.edu>
2023-12-07 15:11:27 -0800

    removing old files

--- gpu_polyfield/example_sets/run_AB_script.py
+++
@@ -1,9 +0,0 @@
-import  subprocess
-import numpy as np
-
-incr = 0.02
-top = 1
-
-
-for i in np.linspace(incr, top-incr, int(top / incr) - 1):
-    subprocess.run(["python",  "AB_test.py",  "%.2f" % i])
