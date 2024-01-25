b0bd4b8de4d55e2c9be3e50ca244559320cd8eb7
Grant M. Rotskoff <grant@rotskoff.cc>
2024-01-25 09:36:03 -0800

    adding polycomp, test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='polycomp',
+   version='0.0.1',
+   description='Python module using cupy for polymer field theoretic simulation',
+   author='Emmit Pert',
+   author_email='epert@stanford.edu',
+   packages=['polycomp'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
