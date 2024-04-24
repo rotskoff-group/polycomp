b2ecb97d778ad0a6797eb3c2b43457424e0541b6
EmmitPert <epert@stanford.edu>
2024-04-24 12:22:03 -0700

    updating some of the plotting tools

---
+++ figures_2024/test.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+# Get the RGB value for the named color
+salmon_rgb = mcolors.to_rgb('skyblue')
+
+print("RGB value for salmon color:", salmon_rgb)
+
