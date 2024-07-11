0426e3ab297a564231d3364eeb4729f1044c0239
EmmitPert <epert@stanford.edu>
2024-07-11 13:56:06 -0700

    fixing some neural ansatz things

--- polycomp/neural_ansatz.py
+++ polycomp/neural_ansatz.py
@@ -4,7 +4,7 @@ import cupy as cp
 from polycomp.ft_system import * 
 from neuralop.models import TFNO
 
-
+import time
 
 
 
@@ -22,7 +22,6 @@ from neuralop.training.callbacks import BasicLoggerCallback
 
 from pathlib import Path
 
-import time 
 
 def create_ansatz(ps, model_dict):
     where_data = "/home/emmit/Rotskoff/neurop_data/clean_runs/wandb/run-20240606_150402-e2ikiizn/checkpoints/"
@@ -35,7 +34,8 @@ def create_ansatz(ps, model_dict):
 
     ps.nansatz_dict = {}
     for key in model_dict.keys():
-        model = TFNO(in_channels=4, out_channels=2, n_modes=(32, 32), hidden_channels=32, projection_channels=64, factorization='tucker', rank=0.42)
+        #model = TFNO(in_channels=4, out_channels=2, n_modes=(32, 32), hidden_channels=32, projection_channels=64, factorization='tucker', rank=0.42)
+        model = TFNO(in_channels=5, out_channels=3, n_modes=(32, 32), hidden_channels=32, projection_channels=64, factorization='tucker', rank=0.42)
         model = model.to(device)
         model.load_checkpoint(model_dict[key], "model")
         data_processor = torch.load(model_dict[key] + "data_processor.pt")
@@ -51,28 +51,25 @@ def infer(ps):
 #    t0 = time.time()
     ps.phi_all *= 0 
     for poly in ps.nansatz_dict.keys():
-
+        if ps.poly_dict[poly]==0:
+            continue
         #data = data1["x"][0].type(torch.float32).clone().to(device='cuda')
         data = cp.concatenate((ps.w_all, ps.grid.grid), axis=0)
         data = data.real.astype(cp.float32)
         data = torch.as_tensor(data, device = 'cuda')
-
-        out = ps.nansatz_dict[poly][0](data.unsqueeze(0))
+        out = ps.nansatz_dict[poly][0](data.unsqueeze(0)) # ~85% of time
     #    out, stupid = data_processor.postprocess(out, data1)
         out = ps.nansatz_dict[poly][1].out_normalizer.inverse_transform(out)
-
         #print("Means", torch.mean(out[0], dim=(1,2)))
         #print("Mins", torch.amin(out[0], dim=(1,2)))
     #    print(torch.mean(out[0,1]))
         out = cp.asarray(out.squeeze().detach())
         out = out.astype(cp.complex128)
         for mon in set(poly.struct.tolist()):
-            amt = cp.sum(poly.h_struct[poly.struct==mon]) * ps.poly_dict[poly]
+            amt = poly.mon_mass[mon] * ps.poly_dict[poly]
             index = ps.monomers.index(mon)
-            ps.phi_all[index] += out[index] / cp.average(out[index]) * amt 
+            ps.phi_all[index] += out[index] / cp.average(out[index]) * amt # ~5% of time
             #TODO: maks sure this is definitely right
-
-    #    print(torch.mean(out[0,1]))
 
 #        hold_avg = cp.average(ps.phi_all, axis=(1,2))
 #        ps.phi_all = out.astype(cp.complex128)
@@ -81,7 +78,6 @@ def infer(ps):
 #    print(cp.amin(ps.phi_all.real, axis=(1,2)))
 
 #    print("Pass")
-#    print(-t0+time.time())
     return
 
 def self_contained():
