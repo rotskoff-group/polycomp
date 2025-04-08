2fda50bb30bfa8fc4b4931eeb0cb71229d9f163a
EmmitPert <epert@stanford.edu>
2025-04-07 17:10:58 -0700

    for publication

--- polycomp/circle_function.py
+++ polycomp/circle_function.py
@@ -218,7 +218,7 @@ def count_corners(center, radius, grid):
     return ind
 
 
-def draw_sphere(grid1, radius, center=cp.array([0.0,0.0,0.0]), upsampling=10):
+def draw_sphere(grid1, radius, center=cp.array([0.0,0.0,0.0]), upsampling=50000000):
     corners = count_corners(center, radius, grid1)
     values = corners.get()
 
@@ -226,7 +226,7 @@ def draw_sphere(grid1, radius, center=cp.array([0.0,0.0,0.0]), upsampling=10):
 
     sphere = cp.zeros_like(grid1.k2)
     hold_sphere = cp.zeros_like(grid1.k2)
-    polar_samples = cp.random.uniform(0, 1, (grid1.k2.size * upsampling,3))
+    polar_samples = cp.random.uniform(0, 1, (upsampling,3))
 
 
     polar_samples[:,0] = polar_samples[:,0]**(1/2)
@@ -238,12 +238,12 @@ def draw_sphere(grid1, radius, center=cp.array([0.0,0.0,0.0]), upsampling=10):
 
 
 
-    samples = cp.random.randn(*(grid1.k2.size * upsampling,3))
+    samples = cp.random.randn(*(upsampling,3))
 
     samples/= cp.linalg.norm(samples, axis=1, keepdims=True)
 
 
-    radius_samples = cp.random.uniform(((radius - cp.sqrt(cp.sum(grid1.dl**2)))/radius)**(3), 1, (grid1.k2.size * upsampling,1))
+    radius_samples = cp.random.uniform(((radius - cp.sqrt(cp.sum(grid1.dl**2)))/radius)**(3), 1, (upsampling,1))
 #    radius_samples = cp.random.uniform(((radius - cp.sqrt(cp.sum(grid1.dl**2)))/radius)**(3) * 0, 1, (grid1.k2.size * upsampling,1))
 
     radius_samples[:,0] = radius_samples[:,0]**(1/3)
@@ -287,17 +287,16 @@ def draw_sphere(grid1, radius, center=cp.array([0.0,0.0,0.0]), upsampling=10):
     sphere[corners==8] = 1
     return sphere
 
-def draw_shell(grid1, radius, center=cp.array([0.0,0.0,0.0]), upsampling=10):
+def draw_shell(grid1, radius, center=cp.array([0.0,0.0,0.0]), upsampling=1000000):
     corners = count_corners(center, radius, grid1)
     values = corners.get()
 
 
 
-    upsampling = 8
     shell = cp.zeros_like(grid1.k2)
     hold_shell = cp.zeros_like(grid1.k2)
 
-    samples = cp.random.randn(*(grid1.k2.size * upsampling,3))
+    samples = cp.random.randn(*(upsampling,3))
 
     samples/= cp.linalg.norm(samples, axis=1, keepdims=True)
 
